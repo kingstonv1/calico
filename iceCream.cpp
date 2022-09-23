b0dd827e0a921ff634f8f4861edb97ac136c2913
kingstonv1 <kingstongise@gmail.com>
2022-09-23 14:41:08 -0400

    first

---
+++ iceCream.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+    using namespace std;
+
+int solve(double i) 
+{
+    return (int) (0.5 * (sqrt(8 * i + 1) - 1));
+}
+
+int main() 
+{
+    int cases;
+    cin >> cases;
+    for (int i = 0; i < cases; i++)
+    {
+        double N;
+        cin >> N;
+        cout << solve(N) << 'n';
+    }
+}
---
+++ rps.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+
+char beats(char c) 
+{
+    switch (c)
+    {
+        case 'R':
+            return 'P';
+        
+        case 'P':
+            return 'S';
+
+        case 'S': 
+            return 'R';
+        
+        default:
+            return 'R';
+    }
+}
+
+char chooseMove(std::vector<char>& me, std::vector <char>& bot, int i)
+{
+    char botMove {'?'};
+    //Determine the bot's move
+    if (i > 1) 
+    {
+        if (bot.at(i) == me.at(i - 1)) 
+        {
+            botMove = beats(me.at(i));
+        }
+        else if (bot.at(i) == bot.at(i - 1)) 
+        {
+            botMove = beats(beats(bot.at(i)));
+        }
+    }
+    //Determine our response
+    return beats(botMove);
+}
+
+int main() 
+{
+    int T;
+    std::vector<char> me;
+    std::vector<char> bot;
+
+    std::cin >> T;
+    
+
+    for(int i = 0; i < T; i++) 
+    {
+        char botMove = 'R';
+        char myMove;
+        myMove = chooseMove(me, bot, i - 1);
+        std::cout << myMove;
+        std::cin >> botMove;
+        bot.push_back(botMove);
+        me.push_back(myMove);
+    }
+
+
+    return 0;
+}
