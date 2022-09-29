bdc18b21ca0fdd203f91b9195629f3d90b55f79e
kingstonv1 <kingstongise@gmail.com>
2022-09-29 12:14:04 -0400

    working solution

---
+++ decks.cpp
@@ -0,0 +1,25 @@
+//Working, but not bonus set.
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <math.h>
+
+int main()
+{
+    int t;
+    std::vector<long long> out;
+    std::cin >> t;
+
+    for (int i = 0; i < t; i++) 
+    {
+        long long rows;
+        std::cin >> rows;
+        out.push_back(std::ceil((rows * (rows + 1) * 0.5 * 3 - rows) / 54));
+    }
+
+    for (int i: out) 
+        { std::cout << i << '\n'; }
+
+    return 0;
+}
