46deb1536a8117b219ab5edd72089be503d72e58
kingstonv1 <kingstongise@gmail.com>
2023-11-13 19:29:06 -0500

    working allergies

--- python/cc-fa21/allergies.py
+++ python/cc-fa21/allergies.py
@@ -1,41 +1,56 @@
-def solve(tests):
-	negative = list()
-	possible = list()
-	known = list()
+def solve(allergens, tests):
+	known = set()
+	maybe = set()
+	negative = set()
 	
-	# Filter out all negatives
-	for i in tests:
-		x = i.split()
-		if x[1] == 'NEGATIVE':
-			for allergen in x[2].split('/'):
-				negative.append(allergen)
-			tests.remove(i)
+	n_tests = [t for t in tests if not t[0]]
+	p_tests = [t for t in tests if t[0]]
 	
-	for i in tests:
-		tested = i.split()[2].split('/')
-		pos = list(set(tested).difference(set(negative)))
-		if len(pos) == 1:
-			known.append(pos[0])
-		else:
-			for x in pos:
-				possible.append(x)
+	# First pass extracts negative results
+	for test in n_tests:
+		negative.update(test[1])
 	
-	possible = list(set(possible).difference(set(known)))
+	# Second pass "cleans" the negatives out
+	for test in p_tests:
+		test[1].difference_update(negative)
 	
-	return f'KNOWN allergens: {"NONE" if len(known) == 0 else "/".join(set(known))}\n' \
-		+ f'POTENTIAL allergens: {"None" if len(possible) == 0 else "/".join(set(possible))}'
+	# Third pass sorts allergens into guaranteed positives and knowns
+	for test in p_tests:
+		if len(test[1]) == 1:
+			known.update(test[1])
+			continue
 
+		maybe.update(test[1])
 
-for _ in range(int(input())):
-	tests = list()
-	numbers = input().split()
-	n = int(numbers[0])
-	m = int(numbers[1])
+	maybe.difference_update(known)
+	allergens.difference_update(negative.union(maybe.union(known)))
+	maybe.update(allergens)
 	
-	allergens = input()
+	known = list(known)
+	maybe = list(maybe)
+	known.sort()
+	maybe.sort()
 	
-	for _ in range(n):
-		tests.append(input())
-	
-	print(solve(tests))
+	print(f'KNOWN allergens: {"/".join(known) if known else "NONE"}')
+	print(f'POTENTIAL allergens: {"/".join(maybe) if maybe else "NONE"}')
 	print()
+	
+
+def main():
+	for _ in range(int(input())):
+		num_tests = int(input().split()[0])
+		allergens = set(input().split())
+		tests = []
+		
+		for _ in range(num_tests):
+			i = input().split()
+			
+			# appends a tuple in the format(positive (True) or negative (False), test_string)
+			tests.append([i[1] == "POSITIVE", set(i[2].split('/'))])
+		
+		solve(allergens, tests)
+		
+		input()
+		
+
+main()
